7f9595d00f7e863eedfc65e02b322f5789b901bc
DenisNSK <muhadenis06@gmail.com>
2021-04-13 11:39:38 +0700

    rrr

---
+++ Блок схема/Блок схема/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Блок_схема
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+            Console.WriteLine("Введите первое число");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите число");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            if (a<(b+c)& b<(c+a)& c<(a+b))
+
+                
+
+            {
+                Console.WriteLine("Существует треугольник");
+            }
+            else
+            {
+                Console.WriteLine("Треугольник не существует");
+            }
+            Console.ReadKey();
+        }
+    }
+}
