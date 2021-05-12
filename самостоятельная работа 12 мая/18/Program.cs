6eb7f3be7e7d14d05d280b239e6e5a8d485a1224
DenisNSK <muhadenis06@gmail.com>
2021-05-12 12:26:41 +0700

    job

---
+++ самостоятельная работа 12 мая/18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("длин l");
+            int l= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("плотность p");
+            int p= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("толщина стенок a");
+            int a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("внутренний радиус r1");
+            int r1 = Convert.ToInt32(Console.ReadLine());
+            int r2 = (a + r1);
+            double v=l*Math.PI*(Math.Pow(r2, 2) - Math.Pow(r1, 2));
+            double d=(r1+2*a);
+            double s = Math.PI / 4 * (Math.Pow(d, 2) - Math.Pow(a, 2));
+            double V = (s * l);
+            double m = (p * v);
+            Console.WriteLine(V);
+            Console.WriteLine(m);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ самостоятельная работа 12 мая/самостоятельная работа 12 мая/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace самостоятельная_работа_12_мая
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("катет a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("катет b");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("толщина h");
+            int h = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("масса m");
+            int m = Convert.ToInt32(Console.ReadLine());
+            double s = ((a * b)*1/2);
+            double v = (s * h);
+            double p = (m * v);
+            Console.WriteLine(p);
+            Console.ReadKey();
+
+        }
+    }
+}
