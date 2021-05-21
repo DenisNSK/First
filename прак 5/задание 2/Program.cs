f2a421e5dbace4f6f76f923bc6ef10e256f7c8f9
DenisNSK <muhadenis06@gmail.com>
2021-05-21 11:25:32 +0700

    natural

---
+++ прак 5/задание 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            int i = 100;
+            while (n <= 10)
+            {
+                if (i % 10 == 7 & i % 9 == 0)
+                {
+                    n++;
+                    Console.WriteLine(i);
+                }
+                
+                i++;
+            }
+            Console.ReadKey();
+                
+
+            }
+        }
+    }
+
---
+++ прак 5/прак 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace прак_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("натурал");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int n = 0;
+            while (x!= 0)
+            {
+                int a = x % 10;
+                if (a % 2 == 0)
+                {
+                    n++;
+                }
+                x = x / 10;
+            } 
+            Console.WriteLine(n);
+            Console.ReadKey();
+
+            
+        }
+    }
+ }
+
